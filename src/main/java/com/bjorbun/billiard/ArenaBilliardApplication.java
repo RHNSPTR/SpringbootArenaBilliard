452464d990531b6ee99747ede79a0ca55c40a326
Rehan Saputra <rs1981773@gmail.com>
2026-07-15 08:56:41 +0700

    feat:Menambahkan EnableVaadin

--- src/main/java/com/bjorbun/billiard/ArenaBilliardApplication.java
+++ src/main/java/com/bjorbun/billiard/ArenaBilliardApplication.java
@@ -2,12 +2,14 @@ package com.bjorbun.billiard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.spring.annotation.EnableVaadin;
 
 @SpringBootApplication
+
+@EnableVaadin({"com.bjorbun.billiard.view", "com.bjorbun.billiard"}) 
 public class ArenaBilliardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArenaBilliardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ArenaBilliardApplication.class, args);
+    }
 }
